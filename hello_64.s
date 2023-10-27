3777b5906ea06ec256d8076a95618d1770b4bb07
Maciej Max <99397154+MaciejMax@users.noreply.github.com>
2023-10-28 01:05:06 +0200

    Initial commit

---
+++ hello_64.s
@@ -0,0 +1,38 @@
+# kernel 
+# https://github.com/torvalds/linux/blob/master/arch/x86/entry/entry_64.S
+# https://github.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
+#
+# Searchable Linux Syscall Table for x86 and x86_64
+# https://filippo.io/linux-syscall-table/
+
+
+# x86-64 Linux System Call convention
+# https://refspecs.linuxfoundation.org/elf/x86_64-abi-0.99.pdf
+# A.2 AMD64 Linux Kernel Conventions
+#
+# A system-call is done via the syscall instruction.
+# The number of the syscall has to be passed in register %rax.
+# The kernel interface uses %rdi, %rsi, %rdx, %r10, %r8 and %r9.
+# The kernel destroys registers %rcx and %r11.
+ 
+
+.section .text
+.global _start
+
+_start:
+    # write message
+    mov $1, %rax          # syscall sys_write
+    mov $1, %rdi          # file descriptor 1 (stdout)
+    mov $msg, %rsi        # pointer to the message
+    mov $len, %rdx        # length
+    syscall
+
+    # exit
+    mov $60, %rax           # syscall sys_exit
+    xor %rdi, %rdi          # exit status, 0
+    syscall
+
+.section .data
+msg:
+    .string "Hello world!\n"
+len = . - msg
